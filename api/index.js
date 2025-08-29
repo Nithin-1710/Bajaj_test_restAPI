59167d0dfa082be6bf1d7352c002db8f9efdc0dd
Nithin-1710 <nithin.weeb@gmail.com>
2025-08-29 11:21:37 +0530

    Refactored for Vercel deployment: moved code to api/index.js, removed app.listen

--- api/index.js
+++ api/index.js
@@ -208,10 +208,4 @@ app.use('*', (req, res) => {
         error: "Endpoint not found"
     });
 });
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    console.log(`API endpoint: http://localhost:${PORT}/bfhl`);
-    console.log(`Test endpoint: http://localhost:${PORT}/test`);
-    console.log(`Health check: http://localhost:${PORT}/`);
-});
+module.exports = app;
